490480fba88026eca147b158478ea1282ed9fa84
vikas9604 <159750474+vikas9604@users.noreply.github.com>
2024-02-16 10:58:32 +0000

    math and nums

---
+++ 01_basic/07_nums_and_math.js
@@ -0,0 +1,47 @@
+const score = 400
+
+//console.log(score);
+
+const balance = new Number (100)
+
+//console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(3));
+
+
+const otherNumber = 324.8554
+
+// console.log(otherNumber);
+
+// console.log(otherNumber.toPrecision(3));
+
+const hudereds = 10000000
+
+//console.log(hudereds.toLocaleString('en-IN'));
+
+
+
+//**************************************maths************************************** */
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(5.3));  //highest value
+// console.log(Math.floor(4.2));  //lowest value
+// console.log(Math.min(4, 5, 6, 9));
+// console.log(Math.max(4, 5, 6, 9));
+
+
+console.log(Math.random());   // to get value 0 and 1
+
+console.log(Math.random()*10 + 1);
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1 )))
