4bb462672e837a9554aac90e23fca89cdc14cea8
vikas9604 <159750474+vikas9604@users.noreply.github.com>
2024-02-16 07:33:42 +0000

    add comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log( 4 > 3);
+// console.log( 4 < 3);
+// console.log( 4 == 3);
+// console.log( 4 >= 3);
+// console.log( 4 <= 3);
+// console.log( 4 != 3);
+
+//***this type of  conversion will do confuse ****//
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+//===
+
+console.log( "2" === 2);
