bdbfdccf6a32bc1575d8607fa30a8bf33be8137a
vikas9604 <159750474+vikas9604@users.noreply.github.com>
2024-02-16 06:45:49 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "dbd655"
+
+// console.log (typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log (valueInNumber)
+
+
+//"33" => 33
+//"33abd" => NaN  (not a number)
+//true => 1; false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 => false
+//" " => false
+//"vikas" => true
+
+let someNumebr = 33
+
+let stringNumber = String(someNumebr)
+
+console.log(stringNumber);
