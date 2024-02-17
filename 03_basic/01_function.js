551a6871c15d1ab2838cf6b38abda1b7348dab44
vikas9604 <159750474+vikas9604@users.noreply.github.com>
2024-02-17 12:57:49 +0000

    add some changes

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -35,4 +35,34 @@ function loginUserMessage (username = "sam") {
 
 // console.log(loginUserMessage("Vikas"));
 
-console.log(loginUserMessage("Vikas"));
+//console.log(loginUserMessage("Vikas"));
+
+
+function calculateCartPrice (val1, val2, ...num1) {
+ return num1
+}
+
+//console.log(calculateCartPrice (200, 400, 600));
+
+const user = {
+    username : "Vikas",
+    price : 200
+}
+
+function  handleObject(anyObject) {
+  console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username : "sam",
+    price : 400
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
