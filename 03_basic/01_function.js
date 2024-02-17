dbb0d285163f2ed15a1d3216aee460ec9accbe82
vikas9604 <159750474+vikas9604@users.noreply.github.com>
2024-02-17 12:33:48 +0000

    add function.js file

---
+++ 03_basic/01_function.js
@@ -0,0 +1,38 @@
+function sayMyName ()  {
+    console.log("V");
+    console.log("I");
+    console.log("K");
+    console.log("A");
+    console.log("S");
+} 
+
+//sayMyName ()
+
+// function addTwoNumber (number1, number2) {
+//     console.log(number1 * number2);
+// }
+
+function addTwoNumber (number1, number2) {
+
+//    let result = number1 + number2
+//    return result
+
+return number1 + number2
+}
+
+
+const result = addTwoNumber (5 , 6)
+
+//console.log("Result :  ", result);
+
+function loginUserMessage (username = "sam") {
+    if(!username) {
+         console.log("please enter a username");
+         return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Vikas"));
+
+console.log(loginUserMessage("Vikas"));
