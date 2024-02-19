9f3e12e47c5bb088824265d69672f09d88b05d53
vikas9604 <159750474+vikas9604@users.noreply.github.com>
2024-02-19 05:11:19 +0000

    learned for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,60 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 50) {
+       // console.log("50 is the best number");
+    }
+
+   // console.log(element);
+    
+}
+
+for (let i = 0; i <= 10; i++) {
+   // console.log(`Outer loop value ${i}`);
+  for (let j = 0; j <=10; j++) {
+  // console.log(`Inner loop value ${j} and inner loop value ${i} `);
+   // console.log(i + '*' + j + ' = ' + i*j );
+  }
+    
+}
+
+
+let myArray = [ "flash", "batman", "superman"]
+
+//console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+
+
+    const element = myArray[index];
+
+   // console.log(element);
+    
+}
+
+
+// break and continue
+
+// for (let i = 1 ; i <= 20 ; i++) {
+
+//     if (i == 5) {
+//         console.log(`Detected 5 `);
+
+//     break
+//     }
+
+//  console.log(`Value of i is ${i}`);
+    
+// }
+
+
+for (let i = 1 ; i <= 20 ; i++) {
+
+    if (i == 5) {
+        console.log(`Detected 5 `);
+        continue
+    }
+
+ console.log(`Value of i is ${i}`);
+    
+}
